1cc11ea4beafdf806b9e5218d4530b74938edc05
Eduardo Monforte <eduardomonforte@gmail.com>
2020-11-03 10:38:22 -0600

    added new font.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.scss";
-import DancingSkeleton from "./components/DancingSkeleton/DancingSkeleton";
 import Fog from "./components/Fog/Fog";
 
 function App() {
   return (
     <>
       <Fog></Fog>
-      <div className="App">{/* <DancingSkeleton></DancingSkeleton> */}</div>
+      <div className="App">Happy Kellyween</div>
     </>
   );
 }
