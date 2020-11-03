cfd8e9f5cad885c73a81bd9d91ba86358b107fdf
Eduardo Monforte <eduardomonforte@gmail.com>
2020-11-03 11:58:54 -0600

    made website responsive, added iframe, change description text.

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,23 @@ function App() {
       <div className="App">
         <p className="AppTitle">Happy Kellyween!</p>
         <p className="AppDescription">
-          <span className="Italic">Spooky season</span> podrá haber terminado,
-          pero las sorpresas seguirán llegando.
+          Gracias por hacer de estos últimos meses los mejores de la cuarentena.
         </p>
+        <p className="AppDescription">
+          <span className="Italic">Spooky season</span> podrá haber terminado,
+          pero las sorpresas apenas están empezando. 👻
+        </p>
+        <div className="IframeContainer">
+          <iframe
+            title="spotify"
+            src="https://open.spotify.com/embed/playlist/1ncjMjObE9sv0K5PocNLWe"
+            frameBorder="0"
+            allowTransparency={true}
+            width="300"
+            height="380"
+            allow="encrypted-media"
+          ></iframe>
+        </div>
       </div>
     </>
   );
